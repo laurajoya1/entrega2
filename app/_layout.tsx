6d905ecb0d247986e8dcf969842fbe3743e91258
laurajoya1 <laurajoya874@gmail.com>
2024-08-06 10:37:04 -1200

    entrega4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
-import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router"
 
-export default function RootLayout() {
-  return (
+const RootLayout = () => {
+  return(
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false  }} />
+      
     </Stack>
-  );
+  )
 }
+
+ 
+
+export default RootLayout;
